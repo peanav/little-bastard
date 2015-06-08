e05ead2872f751e3a9afb2559505a815620e2590
Paul Navasard <paul.navasard@domo.com>
2015-06-08 06:52:29 -0600

    Adding front end rest tester

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 var uuid = require('node-uuid');
 var conf = require('nconf');
 var bastard = require('./bastard');
-//var PostgresBastard = require('./app/postgres/postgresBastard');
+var PostgresBastard = require('./app/postgres/postgresBastard');
 var RethinkBastard = require('./app/rethinkdb/RethinkBastard');
 
 conf.argv().env().file('./config/' + conf.get('NODE_ENV')  + '.json');
@@ -14,20 +15,29 @@ app.use(express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
-  genid: function(req) { return uuid.v4(); },
-  resave: false,
-  saveUninitialized: false,
+  store: new FileStore(),
   secret: conf.get('SESSION_SECRET')
 }));
 
 
-//var postgres = new PostgresBastard('postgres://localhost/test');
+var postgres = new PostgresBastard('postgres://localhost/test');
 var rethink = new RethinkBastard();
-app.use('/api/v1', bastard.db(rethink));
-//app.use(bastard.login(postgres));
+//app.use('/api/v2', bastard.db(postgres));
+app.use('/api/v1/', bastard.db(rethink));
+app.use(bastard.login(rethink));
 
 
 var port = conf.get('PORT') || 3000;
 var server = app.listen(port, function() {
   console.log("Lil' Bastard is running on port " + port);
 });
+
+
+//Cookie Session Stuff
+//app.use(session({
+  //genid: function(req) { return uuid.v4(); },
+  //resave: false,
+  //saveUninitialized: false,
+  //secret: conf.get('SESSION_SECRET')
+//}));
+
